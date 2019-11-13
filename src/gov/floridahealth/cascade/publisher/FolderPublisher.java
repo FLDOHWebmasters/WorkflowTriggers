6a315e3a563dc84775a370e17b9b002c2b99b614
DOH Webmasters <webmasters@flhealth.gov>
2019-11-13 10:02:31 -0500

    auto format java code in Eclipse (no other changes)

--- src/gov/floridahealth/cascade/publisher/FolderPublisher.java
+++ src/gov/floridahealth/cascade/publisher/FolderPublisher.java
@@ -23,164 +23,151 @@ import com.hannonhill.cascade.model.workflow.adapter.PublicWorkflowAdapter;
 
 import gov.floridahealth.cascade.properties.CascadeCustomProperties;
 
-public class FolderPublisher extends Publisher
-{
-  private static final Logger LOG = Logger.getLogger(FolderPublisher.class);
-  private static final String JSON_LOCATION_PROP = "json.defaultFolder";
-  
-  /**
-   * Main function of the Workflow Trigger
-   * 1.) Checks to see if a folder is specified.  If so, it uses it.  If not, it defaults to the JSON folder.
-   * 2.) Checks to see if a site is specified for the folder.  If it does not exist, defaults to the Site for the Asset
-   *     being modified by the Workflow.
-   * 3.) Constructs a Publish Request based upon the options and initiates it.  
-   * @return boolean Successful Completion of Folder publish 
-   */
-  public boolean process()
-    throws TriggerProviderException
-  {
-	 Properties cascadeProperties = null;
-	 String jsonLocation = null;
-	  try {
-    	cascadeProperties = CascadeCustomProperties.getProperties();
-    } catch (IOException ioe) {
-    	throw new TriggerProviderException(ioe.getMessage());
-    }
-    if (cascadeProperties != null ) {
-    	jsonLocation = cascadeProperties.getProperty(JSON_LOCATION_PROP);
-    }
-	  
-	String folderLocation = getParameter("folder");
-    String siteName = getParameter("site");
-    Workflow commonWorkflow;
-    try
-    {
-    	commonWorkflow = ((PublicWorkflowAdapter)this.workflow).getWorkflow();
-    }
-    catch (Throwable t)
-    {
-      String err = String.format("Could not get commonWorkflow for workflow %s", new Object[] { this.workflow.getName() });
-      LOG.fatal(err, t);
-      throw new FatalTriggerProviderException(err, t);
-    }
-    
-    String relatedEntityId = commonWorkflow.getRelatedEntityId();
-    String siteId;
-    if ((siteName != null) && (siteName != "")) {
-      siteId = GetSiteIdFromName(siteName);
-    } else {
-      siteId = GetSiteIdFromAsset(relatedEntityId);
-    }
-    if (siteId != null)
-    {
-      String path = "";
-	  FolderContainedEntity fce;
-      if ((folderLocation != null) && (folderLocation != ""))
-      {
-        path = folderLocation;
-        fce = TryGetFolder(siteId, path);
-      }
-      else if (jsonLocation != null) 
-      {
-        path = jsonLocation;
-        fce = TryGetFolder(siteId, path);
-      } else {
-    	  return false;
-      }
-      Folder folder = (Folder)fce;
-      
-      PublishRequest pubReq = new PublishRequest();
-      /* 
-       * No, I do not know why it needs the ID of the folder and the Folder object of what it's publishing.
-       * What I do know is that without both, it throws up.  C'est la vie.
-       */
-      pubReq.setId(fce.getId());
-      pubReq.setFolder(folder);      
-      pubReq.setPublishAllDestinations(true);
-      pubReq.setGenerateReport(false);
-      pubReq.setUsername("_publisher");
-      
-      /* Kick off the publish */
-      PublishService pubServ = this.serviceProvider.getPublishService();
-      try
-      {
-        pubServ.queue(pubReq);
-      }
-      catch (Exception e)
-      {
-        throw new TriggerProviderException(e.getLocalizedMessage());
-      }
-      return true;
-    }
-    return false;
-  }
-  
-  /**
-   *  Attempts to retrieve the folder object (which is itself a FolderContainedEntity)
-   * @param siteId Site ID of the Folder
-   * @param path Path of the folder
-   * @return FolderContainedEntity object to publish
-   */
-  private FolderContainedEntity TryGetFolder(String siteId, String path)
-  {
-	FolderContainedEntity folder;
-	try
-    {
-      LOG.info("Getting folder id");
-      folder = this.serviceProvider.getLocatorService().locateFolderContainedEntity(path, EntityTypes.TYPE_FOLDER, siteId);
-    }
-    catch (Exception e)
-    {
-      LOG.error("Could not get the designated Folder");
-      return null;
-    }
-    
-    return folder;
-  }
-  
-  /**
-   * Obtains the FolderContainedEntity object represenatation of the asset, then pulls the Site ID from that.
-   * @param assetId ID of the Asset in the associated Workflow 
-   * @return Site ID of the Asset
-   */
-  private String GetSiteIdFromAsset(String assetId)
-  {
-    FolderContainedEntity fce = this.serviceProvider.getLocatorService().locateFolderContainedEntity(assetId);
-    if (fce == null)
-    {
-      LOG.error(String.format("Asset could not be found, ID: %s", new Object[] { assetId }));
-      return null;
-    }
-    return SiteUtil.getSiteId(fce);
-  }
-  
+public class FolderPublisher extends Publisher {
+	private static final Logger LOG = Logger.getLogger(FolderPublisher.class);
+	private static final String JSON_LOCATION_PROP = "json.defaultFolder";
+
 	/**
-	 * Pulls the Site ID based upon the parameter name if passed 
+	 * Main function of the Workflow Trigger 1.) Checks to see if a folder is
+	 * specified. If so, it uses it. If not, it defaults to the JSON folder. 2.)
+	 * Checks to see if a site is specified for the folder. If it does not exist,
+	 * defaults to the Site for the Asset being modified by the Workflow. 3.)
+	 * Constructs a Publish Request based upon the options and initiates it.
+	 * 
+	 * @return boolean Successful Completion of Folder publish
+	 */
+	public boolean process() throws TriggerProviderException {
+		Properties cascadeProperties = null;
+		String jsonLocation = null;
+		try {
+			cascadeProperties = CascadeCustomProperties.getProperties();
+		} catch (IOException ioe) {
+			throw new TriggerProviderException(ioe.getMessage());
+		}
+		if (cascadeProperties != null) {
+			jsonLocation = cascadeProperties.getProperty(JSON_LOCATION_PROP);
+		}
+
+		String folderLocation = getParameter("folder");
+		String siteName = getParameter("site");
+		Workflow commonWorkflow;
+		try {
+			commonWorkflow = ((PublicWorkflowAdapter) this.workflow).getWorkflow();
+		} catch (Throwable t) {
+			String err = String.format("Could not get commonWorkflow for workflow %s",
+					new Object[] { this.workflow.getName() });
+			LOG.fatal(err, t);
+			throw new FatalTriggerProviderException(err, t);
+		}
+
+		String relatedEntityId = commonWorkflow.getRelatedEntityId();
+		String siteId;
+		if ((siteName != null) && (siteName != "")) {
+			siteId = GetSiteIdFromName(siteName);
+		} else {
+			siteId = GetSiteIdFromAsset(relatedEntityId);
+		}
+		if (siteId != null) {
+			String path = "";
+			FolderContainedEntity fce;
+			if ((folderLocation != null) && (folderLocation != "")) {
+				path = folderLocation;
+				fce = TryGetFolder(siteId, path);
+			} else if (jsonLocation != null) {
+				path = jsonLocation;
+				fce = TryGetFolder(siteId, path);
+			} else {
+				return false;
+			}
+			Folder folder = (Folder) fce;
+
+			PublishRequest pubReq = new PublishRequest();
+			/*
+			 * No, I do not know why it needs the ID of the folder and the Folder object of
+			 * what it's publishing. What I do know is that without both, it throws up.
+			 * C'est la vie.
+			 */
+			pubReq.setId(fce.getId());
+			pubReq.setFolder(folder);
+			pubReq.setPublishAllDestinations(true);
+			pubReq.setGenerateReport(false);
+			pubReq.setUsername("_publisher");
+
+			/* Kick off the publish */
+			PublishService pubServ = this.serviceProvider.getPublishService();
+			try {
+				pubServ.queue(pubReq);
+			} catch (Exception e) {
+				throw new TriggerProviderException(e.getLocalizedMessage());
+			}
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Attempts to retrieve the folder object (which is itself a
+	 * FolderContainedEntity)
+	 * 
+	 * @param siteId Site ID of the Folder
+	 * @param path   Path of the folder
+	 * @return FolderContainedEntity object to publish
+	 */
+	private FolderContainedEntity TryGetFolder(String siteId, String path) {
+		FolderContainedEntity folder;
+		try {
+			LOG.info("Getting folder id");
+			folder = this.serviceProvider.getLocatorService().locateFolderContainedEntity(path, EntityTypes.TYPE_FOLDER,
+					siteId);
+		} catch (Exception e) {
+			LOG.error("Could not get the designated Folder");
+			return null;
+		}
+
+		return folder;
+	}
+
+	/**
+	 * Obtains the FolderContainedEntity object represenatation of the asset, then
+	 * pulls the Site ID from that.
+	 * 
+	 * @param assetId ID of the Asset in the associated Workflow
+	 * @return Site ID of the Asset
+	 */
+	private String GetSiteIdFromAsset(String assetId) {
+		FolderContainedEntity fce = this.serviceProvider.getLocatorService().locateFolderContainedEntity(assetId);
+		if (fce == null) {
+			LOG.error(String.format("Asset could not be found, ID: %s", new Object[] { assetId }));
+			return null;
+		}
+		return SiteUtil.getSiteId(fce);
+	}
+
+	/**
+	 * Pulls the Site ID based upon the parameter name if passed
+	 * 
 	 * @param siteName Name of the site containing the asset
 	 * @return Site ID
 	 */
-  private String GetSiteIdFromName(String siteName)
-  {
-    if ((siteName == null) || (siteName.length() == 0))
-    {
-      LOG.error("siteName is null or empty");
-      return null;
-    }
-    Site site = this.serviceProvider.getSiteService().getByName(siteName);
-    if (site == null)
-    {
-      LOG.error(String.format("Site not found %s", new Object[] { siteName }));
-      return null;
-    }
-    return site.getId();
-  }
-  
-  /**
-   * Utility Function required by Cascade
-   * @return always true
-   */
-  public boolean triggerShouldFetchEntity()
-  {
-    return true;
-  }
+	private String GetSiteIdFromName(String siteName) {
+		if ((siteName == null) || (siteName.length() == 0)) {
+			LOG.error("siteName is null or empty");
+			return null;
+		}
+		Site site = this.serviceProvider.getSiteService().getByName(siteName);
+		if (site == null) {
+			LOG.error(String.format("Site not found %s", new Object[] { siteName }));
+			return null;
+		}
+		return site.getId();
+	}
+
+	/**
+	 * Utility Function required by Cascade
+	 * 
+	 * @return always true
+	 */
+	public boolean triggerShouldFetchEntity() {
+		return true;
+	}
 }
--- src/gov/floridahealth/cascade/publisher/ParentFolderPublisher.java
+++ src/gov/floridahealth/cascade/publisher/ParentFolderPublisher.java
@@ -21,89 +21,88 @@ import com.hannonhill.cascade.model.workflow.adapter.PublicWorkflowAdapter;
 import gov.floridahealth.cascade.properties.CascadeCustomProperties;
 
 /**
- * Publishes an ancestor folder of the asset involved in a workflow.
- * Values to use:
- * PARENT - 1 Folder Up
- * GRANDPARENT - 2 Folders Up
- * GREATGRANDPARENT - 3 Folders Up
- * @author VerschageJX 
+ * Publishes an ancestor folder of the asset involved in a workflow. Values to
+ * use: PARENT - 1 Folder Up GRANDPARENT - 2 Folders Up GREATGRANDPARENT - 3
+ * Folders Up (not implemented yet)
+ * 
+ * @author VerschageJX
  */
-public class ParentFolderPublisher
-extends Publisher {
+public class ParentFolderPublisher extends Publisher {
 	private static final Logger LOG = Logger.getLogger(ParentFolderPublisher.class);
-    private static final String PARENT_PARAM_PROP = "parent.param.name";
-    private static final String DEFAULT_PARAM_PROP = "parent.parent.default.value";
-    
-    public boolean process() throws TriggerProviderException {
-    	final String parentParam, defaultValue;
-    	try {
-    		Properties cascadeProperties = CascadeCustomProperties.getProperties();
-    		parentParam = cascadeProperties.getProperty(PARENT_PARAM_PROP);
-    		defaultValue = cascadeProperties.getProperty(DEFAULT_PARAM_PROP);
-    	} catch (IOException ioe) {
-    		throw new TriggerProviderException(ioe.getMessage(), ioe);
-    	}
-    	final String relatedEntityId;
-        try {
-        	Workflow commonWorkflow = ((PublicWorkflowAdapter)this.workflow).getWorkflow();
-        	relatedEntityId = commonWorkflow.getRelatedEntityId();
-        } catch (Throwable t) {
-        	String workflowName = this.workflow == null ? "null" : this.workflow.getName();
-            String err = "Could not get commonWorkflow for workflow " + workflowName;
-            LOG.fatal(err, t);
-            throw new FatalTriggerProviderException(err, t);
-        }
-        LocatorService service = this.serviceProvider.getLocatorService();
-        FolderContainedEntity fce = service.locateFolderContainedEntity(relatedEntityId);
-        if (fce == null) {
-            return fail("Asset could not be found by ID: " + relatedEntityId);
-        }
-        String itemPath = fce.getPath();
-        String siteId = SiteUtil.getSiteId(fce);
-        if (siteId == null) {
-            return fail("Site ID not found for asset: " + itemPath);
-        }
-        String parentType = getParameter(parentParam);
-        if (parentType == null || parentType == "") { parentType = defaultValue; }
-        String parentFolderLocation = itemPath.substring(0, itemPath.lastIndexOf('/'));
-        if (parentType.equals("GRANDPARENT")) {
-        	if (parentFolderLocation.lastIndexOf('/') < 0) {
-                return fail("Cannot publish Grandparent of file in single folder: " + itemPath);
-        	}
-            parentFolderLocation = parentFolderLocation.substring(0, parentFolderLocation.lastIndexOf('/'));
-        } else if (!parentType.equals("PARENT")) {
-            return fail("Parent type invalid: " + parentType);
-        }
-        if (parentFolderLocation == "") {
-            return fail("Missing " + parentType + " folder for asset: " + itemPath);
-        }
-        try {
-        	fce = service.locateFolderContainedEntity(parentFolderLocation, EntityTypes.TYPE_FOLDER, siteId);
-        } catch (Exception e) {
-            return fail("Could not get the designated folder: " + parentFolderLocation + " in site ID " + siteId);
-        }
+	private static final String PARENT_PARAM_PROP = "parent.param.name";
+	private static final String DEFAULT_PARAM_PROP = "parent.parent.default.value";
+
+	public boolean process() throws TriggerProviderException {
+		final String parentParam, defaultValue;
+		try {
+			Properties cascadeProperties = CascadeCustomProperties.getProperties();
+			parentParam = cascadeProperties.getProperty(PARENT_PARAM_PROP);
+			defaultValue = cascadeProperties.getProperty(DEFAULT_PARAM_PROP);
+		} catch (IOException ioe) {
+			throw new TriggerProviderException(ioe.getMessage(), ioe);
+		}
+		final String relatedEntityId;
+		try {
+			Workflow commonWorkflow = ((PublicWorkflowAdapter) this.workflow).getWorkflow();
+			relatedEntityId = commonWorkflow.getRelatedEntityId();
+		} catch (Throwable t) {
+			String workflowName = this.workflow == null ? "null" : this.workflow.getName();
+			String err = "Could not get commonWorkflow for workflow " + workflowName;
+			LOG.fatal(err, t);
+			throw new FatalTriggerProviderException(err, t);
+		}
+		LocatorService service = this.serviceProvider.getLocatorService();
+		FolderContainedEntity fce = service.locateFolderContainedEntity(relatedEntityId);
+		if (fce == null) {
+			return fail("Asset could not be found by ID: " + relatedEntityId);
+		}
+		String itemPath = fce.getPath();
+		String siteId = SiteUtil.getSiteId(fce);
+		if (siteId == null) {
+			return fail("Site ID not found for asset: " + itemPath);
+		}
+		String parentType = getParameter(parentParam);
+		if (parentType == null || parentType == "") {
+			parentType = defaultValue;
+		}
+		String parentFolderLocation = itemPath.substring(0, itemPath.lastIndexOf('/'));
+		if (parentType.equals("GRANDPARENT")) {
+			if (parentFolderLocation.lastIndexOf('/') < 0) {
+				return fail("Cannot publish Grandparent of file in single folder: " + itemPath);
+			}
+			parentFolderLocation = parentFolderLocation.substring(0, parentFolderLocation.lastIndexOf('/'));
+		} else if (!parentType.equals("PARENT")) {
+			return fail("Parent type invalid: " + parentType);
+		}
+		if (parentFolderLocation == "") {
+			return fail("Missing " + parentType + " folder for asset: " + itemPath);
+		}
+		try {
+			fce = service.locateFolderContainedEntity(parentFolderLocation, EntityTypes.TYPE_FOLDER, siteId);
+		} catch (Exception e) {
+			return fail("Could not get the designated folder: " + parentFolderLocation + " in site ID " + siteId);
+		}
 		PublishRequest pubReq = new PublishRequest();
 		pubReq.setId(fce.getId());
-		pubReq.setFolder((Folder)fce);
+		pubReq.setFolder((Folder) fce);
 		pubReq.setPublishAllDestinations(true);
 		pubReq.setGenerateReport(false);
 		pubReq.setUsername("_publisher");
-        PublishService pubServ = this.serviceProvider.getPublishService();
-        try {
-            pubServ.queue(pubReq);
-        } catch (Exception e) {
-            throw new TriggerProviderException(e.getLocalizedMessage(), e);
-        }
-        return true;
-    }
+		PublishService pubServ = this.serviceProvider.getPublishService();
+		try {
+			pubServ.queue(pubReq);
+		} catch (Exception e) {
+			throw new TriggerProviderException(e.getLocalizedMessage(), e);
+		}
+		return true;
+	}
 
-    private boolean fail(String message) {
-    	LOG.error(message);
-    	return false;
-    }
+	private boolean fail(String message) {
+		LOG.error(message);
+		return false;
+	}
 
-    public boolean triggerShouldFetchEntity() {
-        return true;
-    }
+	public boolean triggerShouldFetchEntity() {
+		return true;
+	}
 }
-
